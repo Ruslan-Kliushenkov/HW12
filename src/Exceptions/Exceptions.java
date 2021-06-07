b047557859601cf2673167f2507cb2a465459ce1
Ruslan-Kliushenkov <ruslan.klyshenkov@gmail.com>
2021-06-07 04:12:47 +0300

    Task 2

---
+++ src/Exceptions/Exceptions.java
@@ -0,0 +1,35 @@
+package Exceptions;
+
+public class Exceptions {
+    public static void main(String[] args) {
+
+//Здесь можно обойтись простым if - (else)
+        Object obj = Integer.valueOf(1);
+        String s = "s";
+        if(s.getClass() == obj.getClass()) {
+            s = (String) obj;
+        }else {
+            System.out.println("...");
+        }
+//Здесь так же
+        String str = null;
+        if (str != null) {
+            str.equals(str);
+        }else {
+            System.out.println("...");
+        }
+//...
+        try {
+            int i = Integer.parseInt("s");
+        }catch (NumberFormatException n){
+            System.out.println(n.getMessage());
+        }
+
+        try {
+            int[] array = new int[1];
+            int num = array[1];
+        }catch(IndexOutOfBoundsException i){
+            System.out.println(i.getMessage());
+        }
+    }
+}
--- src/FileReader/Demo.java
+++ src/FileReader/Demo.java
@@ -1,28 +1,24 @@
 package FileReader;
 
 import java.io.*;
-import java.util.Scanner;
 
 public class Demo {
     public static String read(String path){
-//открываем файл по пути
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(path));
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(path))) {
             StringBuilder stringBuilder = new StringBuilder();
             String currentString;
-//построчно считываем файл
+
             while ((currentString = reader.readLine()) != null) {
                 stringBuilder.append(currentString);
                 stringBuilder.append(" \n ");
             }
-//закрываем файл
-            reader.close();
-//возвращаем вычитанный текст в строке
             return stringBuilder.toString();
+        } catch (FileNotFoundException f){
+            return f.getMessage();
         } catch (IOException e) {
             e.printStackTrace();
             return e.getMessage();
-
         }
     }
     public static void main(String[] args){
