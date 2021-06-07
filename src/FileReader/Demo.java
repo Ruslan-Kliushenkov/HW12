7909bb36161118f8718a189fbe12fd9339f105ba
Ruslan-Kliushenkov <83512306+Ruslan-Kliushenkov@users.noreply.github.com>
2021-06-07 03:58:48 +0300

    Update Demo.java

--- src/FileReader/Demo.java
+++ src/FileReader/Demo.java
@@ -5,23 +5,25 @@ import java.util.Scanner;
 
 public class Demo {
     public static String read(String path){
-//открываем файл по пути
+
         try {
             BufferedReader reader = new BufferedReader(new FileReader(path));
             StringBuilder stringBuilder = new StringBuilder();
             String currentString;
-//построчно считываем файл
+
             while ((currentString = reader.readLine()) != null) {
                 stringBuilder.append(currentString);
                 stringBuilder.append(" \n ");
             }
-//закрываем файл
-            reader.close();
-//возвращаем вычитанный текст в строке
-            return stringBuilder.toString();
-        } catch (IOException e) {
-            return e.getMessage();
 
+            reader.close();
+
+            return stringBuilder.toString();
+        } catch (FileNotFoundException f){
+            return f.getMessage();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return e.getMessage();
         }
     }
     public static void main(String[] args){
